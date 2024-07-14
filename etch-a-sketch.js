032aec5532d206ece2b901f8eac9e20877b01b0b
SubaniMalla <subanimalla1@gmail.com>
2024-07-14 10:14:47 +0545

    Create a function to change the grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,19 +1,19 @@
-function createGrid(){
+function createGrid(gridNumber=16){
     const container = document.querySelector("#container");
-    for(let i =0;i<16;i++){
+    for(let i =0;i<gridNumber;i++){
         const row =document.createElement("div");
         row.classList.add("row");
-        for(let j = 0; j<16; j++){
+        for(let j = 0; j<gridNumber; j++){
             const box = document.createElement("div");
             box.classList.add("box");
             row.appendChild(box);
         }
         container.appendChild(row);
     }
+    drawOnHover();
+    changeGridSize();
 }
 
-createGrid();
-drawOnHover();
 
 function drawOnHover(){
     const boxes = document.querySelectorAll(".box");
@@ -23,5 +23,22 @@ function drawOnHover(){
         });
     });
 }
-   
+
+function changeGridSize(){
+    let gridNumber;
+    const container=document.querySelector("#container");
+    const gridChange = document.querySelector("#gridChange");
+    const rows=document.querySelectorAll(".row");
+
+    gridChange.addEventListener("click",()=>{
+            rows.forEach((row)=>{
+                container.removeChild(row);
+            });
+        gridNumber=prompt("Enter the number of squares your want");
+        createGrid(gridNumber);
+    });
+}
+
+createGrid();
+
     
