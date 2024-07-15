a546cb27a3f5f2fbb46465d91c367aa0548fb6e2
SubaniMalla <subanimalla1@gmail.com>
2024-07-15 15:55:47 +0545

    Add random colors when hovering

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,6 @@
 function createGrid(gridNumber=16){
+    let width,height;
+    width=height=parseInt(((500/gridNumber)-1));
     const container = document.querySelector("#container");
     for(let i =0;i<gridNumber;i++){
         const row =document.createElement("div");
@@ -6,8 +8,11 @@ function createGrid(gridNumber=16){
         for(let j = 0; j<gridNumber; j++){
             const box = document.createElement("div");
             box.classList.add("box");
+            box.style.width=width+"px";
+            box.style.height=height+"px";
             row.appendChild(box);
         }
+       
         container.appendChild(row);
     }
     drawOnHover();
@@ -16,10 +21,12 @@ function createGrid(gridNumber=16){
 
 
 function drawOnHover(){
+    let color;
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((box)=>{
         box.addEventListener("mouseover", ()=>{
-            box.setAttribute("style","background-color:blue;");
+            color = randomColor();
+            box.style.backgroundColor = `rgb(${color})`;
         });
     });
 }
@@ -31,12 +38,24 @@ function changeGridSize(){
     const rows=document.querySelectorAll(".row");
 
     gridChange.addEventListener("click",()=>{
-            rows.forEach((row)=>{
+        do{
+            gridNumber=parseInt(prompt("Enter the size of the grid max(60)"));
+        }while(gridNumber>60);
+
+        rows.forEach((row)=>{
                 container.removeChild(row);
-            });
-        gridNumber=prompt("Enter the number of squares your want");
-        createGrid(gridNumber);
+        });
+            createGrid(gridNumber);
     });
+}
+function randomColor(){
+    let r,g,b;
+    r=(Math.floor(Math.random()*256));
+    g=(Math.floor(Math.random()*256));
+    b=(Math.floor(Math.random()*256));
+    color=(r + ',' + g + ',' + b);
+    return color;
+    
 }
 
 createGrid();
